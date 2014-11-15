711e6e663c6d31f40552e042dd8d71d9fd922eed
jasperknowsbest <edward.jasper@flatironschool.com>
2014-11-15 16:18:55 -0500

    edit message

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,6 +3,7 @@ class Product < ActiveRecord::Base
 	validates_inclusion_of :price, :in => 1000..10000
 
 	def self.get_suggested_items(original_price, furniture)
+		#SQL query that selects 5 products near 60% of original price and of the same furniture type
   	where("furniture_type = ?", "#{furniture}").order("abs(products.price - #{original_price}/0.60)")
   end
 
